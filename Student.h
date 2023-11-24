710cf52b42b5499f75f9e09309565586007ba7a8
Yasamin Fazliani <yasaminf@users.noreply.github.com>
2023-11-24 15:56:46 -0500

    Update Student.h

--- Student.h
+++ Student.h
@@ -12,7 +12,11 @@ namespace sdds {
         Student(const char* studentName, int studentAge);
         Student(const Student& other);
         Student& operator=(const Student& other);
-        ~Student();
+        virtual ~Student(); //This could be a source of memory leak
+                            //From now on always have "virtual" for your destructors
+                            //It does not affect anything if inheritance is not involved.
+                            //But it does avoid memory leak by using the most updated destructor
+                            //when inheritance is involved. Read about it in your virtuality notes.
         void display() const;
     };
 } // namespace sdds
